b6a8db44990202f57d9a29e5ff96868fc36ef749
yurii-suprun <iursuprun@gmail.com>
2025-10-03 16:05:26 +0200

    Updated infrastructure variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,3 +1,3 @@
-variable "aws_region" { default = "us-east-1" }
+variable "aws_region" { default = "eu-central-1" }
 variable "admin_email" { default = "iursuprun@gmail.com" }
 variable "from_email" { default = "no-reply@example.com" }

723a35387b18ced3adfc0bd60da5fc7f385faa2f
yurii-suprun <iursuprun@gmail.com>
2025-10-05 14:25:05 +0200

    Updated iam policy

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,7 +2,7 @@ variable "aws_region" { default = "eu-central-1" }
 
 variable "admin_email" { default = "iursuprun@gmail.com" }
 
-variable "from_email" { default = "no-reply@example.com" }
+variable "from_email" { default = "iursuprun@gmail.com" }
 
 variable "lambda_package_path" {
   description = "Path to the built Lambda JAR file"

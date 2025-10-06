037b611c87364b76234d909c0d13748f72eb83f0
yurii-suprun <iursuprun@gmail.com>
2025-10-06 13:02:34 +0200

    Updated infrastructure file

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -112,7 +112,7 @@ resource "aws_api_gateway_resource" "comment_resource" {
 }
 
 # -------------------------------
-# POST method
+# POST method (Lambda proxy)
 # -------------------------------
 resource "aws_api_gateway_method" "comment_post" {
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
@@ -121,7 +121,6 @@ resource "aws_api_gateway_method" "comment_post" {
   authorization = "NONE"
 }
 
-# Lambda integration
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.rest_api.id
   resource_id             = aws_api_gateway_resource.comment_resource.id
@@ -129,29 +128,6 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.comment_handler.invoke_arn
-}
-
-# Add CORS header to POST response
-resource "aws_api_gateway_integration_response" "post_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_resource.comment_resource.id
-  http_method = aws_api_gateway_method.comment_post.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-  }
-}
-
-resource "aws_api_gateway_method_response" "post_method_response" {
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_resource.comment_resource.id
-  http_method = aws_api_gateway_method.comment_post.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
 }
 
 # -------------------------------
@@ -165,20 +141,20 @@ resource "aws_api_gateway_method" "comment_options" {
 }
 
 resource "aws_api_gateway_integration" "comment_options_mock" {
-  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
-  resource_id             = aws_api_gateway_resource.comment_resource.id
-  http_method             = aws_api_gateway_method.comment_options.http_method
-  type                    = "MOCK"
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.comment_resource.id
+  http_method   = aws_api_gateway_method.comment_options.http_method
+  type          = "MOCK"
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
   }
 }
 
 resource "aws_api_gateway_integration_response" "options_integration_response" {
-  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-  resource_id   = aws_api_gateway_resource.comment_resource.id
-  http_method   = aws_api_gateway_method.comment_options.http_method
-  status_code   = "200"
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.comment_resource.id
+  http_method = aws_api_gateway_method.comment_options.http_method
+  status_code = "200"
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
